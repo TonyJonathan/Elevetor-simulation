ee5810039060a9b256eb5dc98882cb56169e21a2
tony <tony.bruchon@hotmail.fr>
2024-11-20 00:25:35 +0100

    Configuration update.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Elevator-simulation/', // Remplace <nom-du-repo> par le nom exact de ton dépôt
-});
-
+  plugins: [react()],
+})
